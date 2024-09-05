1659badaba29a886f834d03d9ae545b09d454afa
Killjoy99 <pterror11@gmail.com>
2024-09-06 01:15:04 +0200

    Building with httpx: To enable internet before usage to avoid crashing: Test 1

--- libs/applibs/connection_manager.py
+++ libs/applibs/connection_manager.py
@@ -5,14 +5,10 @@ from httpx import Client, ConnectError
 client = Client()
 
 
-class ConnectionManager:
-    def __init__(self, client: Client) -> None:
-        self.client = client
-
-    def get_google(self):
-        try:
-            response = client.get("https://google.com")
-            return response
-        except ConnectError:
-            logging.warning("Failed to Create a network.")
-            return
+def get_google(client: Client):
+    try:
+        response = client.get("https://google.com")
+        return response
+    except ConnectError:
+        logging.warning("Failed to Create a network.")
+        return
